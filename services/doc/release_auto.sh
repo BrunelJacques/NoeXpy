abed591b5118fe574749ea21f8a8b93ebb830fa3
BrunelJacques <jbrunel@laposte.net>
2024-02-21 17:40:34 +0100

    adaptations pour linux

--- services/doc/release_auto.sh
+++ services/doc/release_auto.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # lancer:$ sudo bash install_auto.sh
-cd /home/noegest
+cd /home/noegest/Noexpy
 #git stash
 #git reset --hard
 git pull https://github.com/BrunelJacques/Noexpy
-cd /home/noegest/Noexpy
 chmod +x ./lancer_noe*.sh
 #sudo chgrp -R noegest /home/noegest/Noexpy
 #sudo chmod 775 -R /home/noegest
